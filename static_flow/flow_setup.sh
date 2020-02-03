c542d9c40b4014f2fa19f77a98479ff84bad3782
petersilva <petersilva@sourceforge.net>
2020-02-03 09:40:10 -0500

    move path spec into flow_setup to make one off tests easier.

--- static_flow/flow_setup.sh
+++ static_flow/flow_setup.sh
@@ -172,8 +172,8 @@ else
     "$SARRA_LIB"/sr_post.py -config t_dd2_f00.conf >$LOGDIR/sr_post_t_dd2_f00_01.log 2>&1 &
 fi
 
-sr_cpost -config pelle_dd1_f04.conf >$LOGDIR/sr_cpost_pelle_dd1_f04_01.log 2>&1 &
-sr_cpost -config pelle_dd2_f05.conf >$LOGDIR/sr_cpost_pelle_dd2_f05_01.log 2>&1 &
+sr_cpost -config pelle_dd1_f04.conf ${TESTSTATICDATA} >$LOGDIR/sr_cpost_pelle_dd1_f04_01.log 2>&1 &
+sr_cpost -config pelle_dd2_f05.conf ${TESTSTATICDATA} >$LOGDIR/sr_cpost_pelle_dd2_f05_01.log 2>&1 &
 
 echo "posting complete: `date +${SR_DATE_FMT}`"
 
