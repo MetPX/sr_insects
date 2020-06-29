419be4997ceeb745289c33a64e2aee55fb850fd8
Tyson Kaufmann <umkaufmt@myumanitoba.ca>
2020-06-29 13:26:30 -0700

    Update flow_autoconfig.sh script
    Removed duplicate flow_autoconfig.sh scripts within flakey_broker,
     dynamic_flow, and static_flow. Created new flow_autoconfig.sh at
     root directory. Updated dependencies fix ssh failing.

--- flow_autoconfig.sh
+++ flow_autoconfig.sh
@@ -1,28 +1,34 @@
 # Flow Test Autoconfig
 #
-# Script not meant to be run on personal machines (may break some configs)
-# Intended use case is a fresh sys (tested on ubuntu18.04desktop)
-# which can easily be run in a virtualbox VM.
+# Script meant to be run on fresh installations. Downloads dependencies
+#  and configures a RabbitMQ broker for running sr_insects tests.
+#
+# WARNING: May break current sarracenia configurations on system.
+#
+# RabbitMQ web console can be accessed at: http://localhost:15672
+# Credentials for log-in are created at: ~/.config/sarra/credentials.conf
+#
+# Tested on Ubuntu 18.04
 
 # Install and configure dependencies
+echo "-- Installing dependencies --"
 sudo apt-key adv --keyserver "hkps.pool.sks-keyservers.net" --recv-keys "0x6B73A36E6026DFCA"
 sudo add-apt-repository -y ppa:ssc-hpc-chp-spc/metpx
 sudo apt-get update
-sudo apt -y install rabbitmq-server erlang-nox sarrac librabbitmq4 libsarrac libsarrac-dev
+sudo apt -y install --no-install-recommends rabbitmq-server erlang-nox sarrac librabbitmq4 libsarrac libsarrac-dev openssh-server net-tools python3-pip
 
 pip3 install -U pip
-pip3 install -e ..
-pip3 install pyftpdlib paramiko 
-echo
+pip3 install pyftpdlib paramiko
 
 # Setup autossh login
+echo "-- Enabling autossh login on localhost --"
 rm ~/.ssh/id_rsa
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
-ssh -oStrictHostKeyChecking=no localhost "echo"
-echo
+ssh -oStrictHostKeyChecking=no localhost "echo ssh connection to localhost successful"
 
 # Setup basic configs
+echo "-- Creating configuration files --"
 mkdir -p ~/.config/sarra
 
 cat > ~/.config/sarra/default.conf << EOF
@@ -55,10 +61,9 @@ declare subscriber tsub
 declare subscriber anonymous
 EOF
 
-echo
-
 # Manage RabbitMQ
-sudo systemctl restart rabbitmq-server
+echo "-- Configuring the RabbitMQ broker --"
+#sudo systemctl restart rabbitmq-server
 sudo rabbitmq-plugins enable rabbitmq_management
 
 sudo rabbitmqctl delete_user guest
@@ -71,15 +76,13 @@ sudo rabbitmqctl add_user bunnymaster ${ADMIN_PASSWORD}
 sudo rabbitmqctl set_permissions bunnymaster ".*" ".*" ".*"
 sudo rabbitmqctl set_user_tags bunnymaster administrator
 
-echo
-
 sudo systemctl restart rabbitmq-server
 cd /usr/local/bin
 sudo mv rabbitmqadmin rabbitmqadmin.1
 sudo wget http://localhost:15672/cli/rabbitmqadmin
 sudo chmod 755 rabbitmqadmin
 
-echo
-
 # Configure users
+echo "-- Configuring users --"
 sr_audit --users foreground
+
