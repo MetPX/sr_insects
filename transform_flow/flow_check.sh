60506323b8e8c9a55a61c0d7ec2c7184d3ec45c1
PeterSilva <peter@bsqt.homeip.net>
2021-04-24 11:05:20 -0400

    Adding transform_flow where the output directory is different from input.

---
+++ transform_flow/flow_check.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+# parse arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -s|--skip_summaries)
+    skip_summaries=true
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+. ./flow_include.sh
+countall
+
+function summarize_performance {
+    path="$LOGDIR"/$1
+    shift
+    pattern=$1
+    shift
+    for i in $* ; do
+       best_fn=''
+       printf "\n\t$i\n\n"
+       for j in ${path}_${i}_*.log; do
+           msg="`grep ${pattern} ${j} | tail -1`"
+           if [[ -z "$msg" ]]; then
+               continue
+           fi
+           fn=`echo $(basename ${j}) | awk -F'.' '{print $3}'`
+           if [[ -z "$fn" ]]; then
+               best_fn=`echo $(basename ${j})`
+               echo "`basename $j` ${msg}"
+           elif [[ -z "$best_fn" ]]; then
+               echo "`basename $j` ${msg}"
+           fi
+       done
+    done
+}
+
+function summarize_logs {
+    printf "\n$1 Summary:\n"
+    input_size=${#1}
+    fcl="$LOGDIR"/flowcheck_$1_logged.txt
+    msg_counts=`grep -h -o "\[$1\] *.*" "$LOGDIR"/*.log | sort | uniq -c -w"$((input_size+20))" | sort -n -r`
+    echo '' > ${fcl}
+
+    if [[ -z $msg_counts ]]; then
+       echo NO $1S IN LOGS
+    else
+       backup_ifs=$IFS
+       IFS=$'\n'
+       for msg_line in $msg_counts; do
+            count=`echo ${msg_line} | awk '{print $1}'`
+            msg=`echo ${msg_line} | sed "s/^ *[0-9]* \[$1\] *//g"`
+            pattern="\[$1\] *${msg}"
+            filelist=($(grep -l ${pattern::$((input_size + 22))} "$LOGDIR"/*.log))
+            if [[ ${filelist[@]} ]]; then
+                first_filename=`basename ${filelist[0]} | sed 's/ /\n/g' | sed 's|.*\/||g' | sed 's/_[0-9][0-9]\.log\|.log//g' | uniq`
+                files_nb=${#filelist[@]}
+                echo "  ${count}"$'\u2620'"${first_filename}"$'\u2620'"(${files_nb} file)"$'\u2620'"`echo ${msg_line} | sed "s/^ *[0-9]* //g"`" >> ${fcl}
+                echo ${filelist[@]} | sed 's/^//g' | sed 's/ \//\n\//g' >> ${fcl}
+                echo -e >> ${fcl}
+            fi
+       done
+       IFS=${backup_ifs}
+       result=`grep -c $1 ${fcl}`
+       if [[ ${result} -gt 10 ]]; then
+           grep $1 ${fcl} | head | column -t -s $'\u2620' | cut -c -130
+           echo
+           echo "More than 10 TYPES OF $1S found... for the rest, have a look at $fcl for details"
+       else
+           grep $1 ${fcl} | column -t -s $'\u2620' | cut -c -130
+       fi
+    fi
+}
+
+if [[ -z "$skip_summaries" ]]; then
+    # PAS performance summaries
+    printf "\nDownload Performance Summaries:\tLOGDIR=$LOGDIR\n"
+    summarize_performance sr_shovel msg_total: t_dd1 t_dd2
+    summarize_performance sr_subscribe file_total: cdnld_f21 cfile_f44 u_sftp_f60 ftp_f70 q_f71
+
+    echo
+    # MG shows retries
+    echo
+
+    if [[ ! "$SARRA_LIB" ]]; then
+       echo NB retries for sr_sender    `grep Retrying "$LOGDIR"/${LGPFX}sender*.log | wc -l`
+    else
+       echo NB retries for "$SARRA_LIB"/sr_sender.py    `grep Retrying "$LOGDIR"/${LGPFX}sender*.log | wc -l`
+    fi
+
+    summarize_logs ERROR
+    summarize_logs WARNING
+fi
+
+passedno=0
+tno=0
+
+if [[ "${totshovel2}" -gt "${totshovel1}" ]]; then
+   maxshovel=${totshovel2}
+else 
+   maxshovel=${totshovel1}
+fi
+printf "\n\tMaximum of the shovels is: ${maxshovel}\n\n"
+
+printf "\t\tTEST RESULTS\n\n"
+
+tot2shov=$(( ${totshovel1} + ${totshovel2} ))
+t4=$(( ${totfileamqp}*4 ))
+
+echo "                 | dd.weather routing |"
+calcres "${staticfilecount}" "${totshovel2}" "sr_post\t count of posted files (${totshovel2}) should be same those in the static data directory\t (${staticfilecount})"
+calcres "${totshovel1}" "${totshovel2}" "sr_post\t (${totshovel1}) t_dd1 should have the same number of items as t_dd2\t (${totshovel2})"
+calcres "${totsarx}" "${tot2shov}" "sr_sarra\t (${totsarx}) should receive the same number of items as both post\t (${tot2shov})"
+calcres "${totsarp}" "${totshovel1}" "sr_sarra\t (${totsarp}) should publish the same number of items as one post\t (${totshovel1})"
+calcres "${totrejected}" "${totshovel1}" "sr_sarra\t (${totrejected}) should reject the same number of items as one post\t (${totshovel1})"
+#calcres "${totfileamqp}" "${totsarp}" "sr_subscribe\t (${totfileamqp}) should rx the same number of items as sarra published\t (${totsarp})"
+echo "                 | watch      routing |"
+#calcres "${totwatch}" "${totfileamqp}"         "sr_watch\t\t (${totwatch}) should be the same as subscribe amqp_f30\t\t  (${totfileamqp})"
+#calcres "${totsent}" "${totwatch}" "sr_sender\t\t (${totsent}) should publish the same number of items as sr_watch  (${totwatch})"
+#calcres "${totsubrmqtt}" "${totwatch}" "rabbitmqtt\t\t (${totsubrmqtt}) should download same number of items as sr_watch  (${totwatch})"
+calcres "${totsubu}" "${totsent}"  "sr_subscribe u_sftp_f60 (${totsubu}) should download same number of items as sr_sender (${totsent})"
+calcres "${totsubcp}" "${totsent}" "sr_subscribe cp_f61\t (${totsubcp}) should download same number of items as sr_sender (${totsent})"
+echo "                 | poll       routing |"
+calcres "${totpoll1}" "${totsent}" "sr_poll f62\t (${totpoll1}) should publish same number of items of sr_sender sent\t (${totsent})"
+calcres "${totsubq}" "${totpoll1}" "sr_subscribe q_f71\t (${totsubq}) should download same number of items as sr_poll test1_f62 (${totpoll1})"
+echo "                 | flow_post  routing |"
+calcres "${totpost1}" "${totsent}" "sr_post test2_f61\t (${totpost1}) should have the same number of items of sr_sender \t (${totsent})"
+calcres "${totsubftp}" "${totpost1}" "sr_subscribe ftp_f70\t (${totsubftp}) should have the same number of items as sr_post test2_f61 (${totpost1})"
+calcres "${totpost1}" "${totshimpost1}" "sr_post test2_f61\t (${totpost1}) should have about the same number of items as shim_f63\t (${totshimpost1})"
+
+echo "                 | py infos   routing |"
+zerowanted "${missed_dispositions}" "${maxshovel}" "messages received that we don't know what happened."
+# check removed because of issue #294
+#calcres ${totshortened} ${totfileamqp} \
+#   "count of truncated headers (${totshortened}) and subscribed messages (${totmsgamqp}) should have about the same number of items"
+
+# these almost never are the same, and it's a problem with the post test. so failures here almost always false negative.
+#calcres ${totpost1} ${totsubu} "post test2_f61 ${totpost1} and subscribe u_sftp_f60 ${totsubu} run together. Should be about the same."
+
+
+tallyres "${tno}" "${passedno}" "Overall ${passedno} of ${tno} passed (sample size: $staticfilecount) !"
+results=$?
+
+if (("${missed_dispositions}">0)); then
+   # PAS missed_dispositions means definite Sarra bug, very serious.
+   echo "Please review $missedreport"
+   results=1
+fi
+echo
+
+exit ${results}
---
+++ transform_flow/flow_include.sh
@@ -0,0 +1,292 @@
+#!/bin/bash
+
+. ../flow_utils.sh
+
+C_ALSO="`which sr_cpost`"
+# The directory we run the flow test scripts in...
+tstdir="`pwd`"
+httpdocroot=`cat $tstdir/.httpdocroot`
+
+if [ "${sarra_py_version:0:1}" == "3" ]; then
+   LGPFX=""
+else
+   LGPFX="sr_"
+fi
+
+function countthem {
+   if [ ! "${1}" ]; then
+      tot=0
+   else
+      tot="${1}"
+   fi
+}
+
+function chkargs {
+
+   if [[ ! "${1}" || ! "${2}" ]]; then
+      printf "test %2d FAILURE: blank results! ${3}\n" ${tno}
+      return 2
+   fi
+   if [[ "${1}" -eq 0 ]]; then
+      printf "test %2d FAILURE: no successful results! ${3}\n" ${tno}
+      return 2
+   fi
+
+   if [[ "${2}" -eq 0 ]]; then
+      printf "test %2d FAILURE: no successful results, 2nd item! ${3}\n" ${tno}
+      return 2
+   fi
+
+   return 0
+}
+
+function calcres {
+   #
+   # calcres - Calculate test result.
+   # 
+   # logic:
+   # increment test number (tno)
+   # compare first and second totals, and report agreement if within some percent of one another.
+   # see maxerr calculation... 
+   # emit description based on agreement.  Arguments:
+   # 1 - first total
+   # 2 - second total 
+   # 3 - test description string.
+   # 4 - will retry flag.
+   #
+   
+   tno=$((${tno}+1))
+
+   chkargs "${1}" "${2}" "${3}"
+   if [ $? -ne 0 ]; then
+      return $?
+   fi
+
+   res=0
+
+   mean=$(( (${1} + ${2}) / 2 ))
+   maxerr=$(( $mean / 1000 ))
+
+   min=$(( $mean - $maxerr ))
+   max=$(( $mean + $maxerr ))
+
+   if [ $1 -lt $min -o $2 -lt $min -o $1 -gt $max -o $1 -gt $max ]; then
+	   printf "test %2d FAILURE: ${3}\n" ${tno}
+      if [ "$4" ]; then
+         tno=$((${tno}-1))
+      fi    
+      return 1
+   else
+      printf "test %2d success: ${3}\n" ${tno}
+      passedno=$((${passedno}+1))
+      return 0
+   fi
+
+}
+
+function tallyres {
+   # tallyres - All the results must be good.  99/100 is bad!
+   # 
+   # logic:
+   # increment test number (tno)
+   # compare first and second totals, and report agreement of one another.
+   # emit description based on agreement.  Arguments:
+   # 1 - value obtained 
+   # 2 - value expected
+   # 3 - test description string.
+
+   tno=$((${tno}+1))
+
+   if [ ${1} -ne ${2} -o ${2} -eq 0 ]; then
+      printf "test %2d FAILURE: ${3}\n" ${tno}
+      if [ "$4" ]; then
+         tno=$((${tno}-1))
+      fi    
+      return 1
+   else
+      printf "test %2d success: ${3}\n" ${tno}
+      passedno=$((${passedno}+1))
+      return 0
+   fi
+
+}
+
+function zerowanted {
+   # zerowanted - this value must be zero... checking for bad things.
+   # 
+   # logic:
+   # increment test number (tno)
+   # compare first and second totals, and report agreement if within 10% of one another.
+   # emit description based on agreement.  Arguments:
+   # 1 - value obtained 
+   # 2 - samplesize
+   # 3 - test description string.
+
+   tno=$((${tno}+1))
+
+   if [ "${2}" -eq 0 ]; then
+      printf "test %2d FAILURE: no data! ${3}\n" ${tno}
+      return
+   fi
+
+   if [ "${1}" -gt 0 ]; then
+      printf "test %2d FAILURE: ${1} ${3}\n" ${tno}
+   else
+      printf "test %2d success: ${1} ${3}\n" ${tno}
+      passedno=$((${passedno}+1))
+   fi
+}
+
+function sumlogs {
+
+  pat="$1"
+  shift
+  tot=0
+  for l in $*; do
+     to_add="`grep -a "\[INFO\] $pat" $l | tail -1 | awk ' { print $5; }; '`"
+     if [ "$to_add" ]; then
+        tot=$((${tot}+${to_add}))
+     fi
+  done
+}
+
+function sumlogshistory {
+  p="$1"
+  shift
+  if [[ $(ls $* 2>/dev/null) ]]; then
+      reverse_date_logs=`ls $* | sort -n -r`
+
+      for l in $reverse_date_logs; do
+         if [[ ${tot} = 0 ]]; then
+             sumlogs $p $l
+         fi
+      done
+  fi
+}
+
+function countall {
+
+  sumlogs msg_total $LOGDIR/${LGPFX}report_tsarra_f20_*.log
+  totsarra="${tot}"
+
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+       countthem "`grep -a 'putNewMessage published' "$LOGDIR"/post_t_dd1_f00_*.log | wc -l`"
+  else
+       countthem "`grep -a '\[INFO\] post_log' "$LOGDIR"/sr_post_t_dd1_f00_*.log | wc -l`"
+  fi
+  totshovel1="${tot}"
+
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+       countthem "`grep -a 'putNewMessage published' "$LOGDIR"/post_t_dd2_f00_*.log | wc -l`"
+  else
+       countthem "`grep -a '\[INFO\] post_log' "$LOGDIR"/sr_post_t_dd2_f00_*.log | wc -l`"
+  fi
+  totshovel2="${tot}"
+
+  countthem "`grep -a rejected  "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | grep -v DEBUG | wc -l`"
+  totrejected="${tot}"
+
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+       countthem "`grep -a 'putNewMessage published' "$LOGDIR"/${LGPFX}watch_f40_*.log | wc -l`"
+  else
+       countthem "`grep -a '\[INFO\] post_log' "$LOGDIR"/${LGPFX}watch_f40_*.log | wc -l`"
+  fi
+  totwatch="${tot}"
+
+#  sumlogs msg_total $LOGDIR/${LGPFX}subscribe_amqp_f30_*.log
+#  totmsgamqp="${tot}"
+
+#  if [ "${sarra_py_version:0:1}" == "3" ]; then
+#      countthem "`grep -a 'do_download downloaded ok' "$LOGDIR"/${LGPFX}subscribe_amqp_f30_*.log | wc -l`"
+#  else
+#      countthem "`grep -a '\[INFO\] file_log downloaded to:' "$LOGDIR"/${LGPFX}subscribe_amqp_f30_*.log | wc -l`"
+#  fi
+#  totfileamqp="${tot}"
+
+  countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}sender_tsource2send_f50_*.log | wc -l`"
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+      countthem "`grep -a 'putNewMessage published' "$LOGDIR"/${LGPFX}sender_tsource2send_f50_*.log | wc -l`"
+  else
+      countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}sender_tsource2send_f50_*.log | wc -l`"
+  fi
+  totsent="${tot}"
+
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+      all_events="do_download\ downloaded\ ok|write_inline_file\ data\ inlined\ with\ message"
+      countthem "`grep -aE "$all_events" "$LOGDIR"/${LGPFX}subscribe_rabbitmqtt_f31_*.log | grep -v DEBUG | wc -l`"
+  else
+      no_hardlink_events='downloaded to:|symlinked to|removed'
+      all_events="hardlink|$no_hardlink_events"
+      countthem "`grep -aE "$all_events" "$LOGDIR"/${LGPFX}subscribe_rabbitmqtt_f31_*.log | grep -v DEBUG | wc -l`"
+  fi
+  totsubrmqtt="${tot}"
+
+  countthem "`grep -aE "$all_events" "$LOGDIR"/${LGPFX}subscribe_u_sftp_f60_*.log | grep -v DEBUG | wc -l`"
+  totsubu="${tot}"
+  countthem "`grep -aE "$all_events" "$LOGDIR"/${LGPFX}subscribe_cp_f61_*.log | grep -v DEBUG | wc -l`"
+  totsubcp="${tot}"
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+      all_events="do_download downloaded ok"
+      countthem "`grep -aE "$all_events" "$LOGDIR"/${LGPFX}subscribe_ftp_f70_*.log | grep -v DEBUG | wc -l`"
+  else
+      countthem "`grep -aE "$no_hardlink_events" "$LOGDIR"/${LGPFX}subscribe_ftp_f70_*.log | grep -v DEBUG | wc -l`"
+fi
+  totsubftp="${tot}"
+  countthem "`grep -aE "$all_events" "$LOGDIR"/${LGPFX}subscribe_q_f71_*.log | grep -v DEBUG | wc -l`"
+  totsubq="${tot}"
+
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+       countthem "`grep -a 'putNewMessage published' "$LOGDIR"/${LGPFX}poll_f62_*.log | wc -l`"
+  else
+       countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}poll_f62_*.log | wc -l`"
+  fi
+  totpoll1="${tot}"
+
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+       countthem "`grep -a 'putNewMessage published' $srposterlog | grep -v shim | wc -l`"
+  else
+       countthem "`grep -a '\[INFO\] post_log notice' $srposterlog | grep -v shim | wc -l`"
+  fi
+  totpost1="${tot}"
+
+  countthem "`grep -a '\[INFO\] published:' $srposterlog | grep shim | wc -l`"
+  totshimpost1="${tot}"
+
+  #countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
+  if [ "${sarra_py_version:0:1}" == "3" ]; then
+      countthem "`grep -a 'after_accept accepted:' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
+      totsarx="${tot}"
+      countthem "`grep -a 'putNewMessage published' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
+      totsarp="${tot}"
+  else
+      countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
+      totsarp="${tot}"
+      countthem "`grep -a 'received' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
+      totsarx="${tot}"
+  fi
+
+  if [[ ! "$C_ALSO" && ! -d "$SARRAC_LIB" ]]; then
+     return
+  fi
+  staticfilecount="`ls -lR ${SAMPLEDATA}/20200105/WXO-DD/bulletins | grep '^-r' | wc -l`"
+
+  if [[ $(ls "$LOGDIR"/${LGPFX}shovel_pclean_f90*.log 2>/dev/null) ]]; then
+      countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}shovel_pclean_f90*.log | wc -l`"
+      totpropagated="${tot}"
+  else
+      totpropagated="0"
+  fi
+
+  if [[ $(ls "$LOGDIR"/${LGPFX}shovel_pclean_f92*.log 2>/dev/null) ]]; then
+      countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}shovel_pclean_f92*.log | wc -l`"
+      totremoved="${tot}"
+  else
+      totremoved="0"
+  fi
+
+  # flags when two lines include *msg_log received* (with no other message between them) indicating no user will know what happenned.
+  awk 'BEGIN { lr=0; }; /msg_log received/ { lr++; print lr, FILENAME, $0 ; next; }; { lr=0; } '  $LOGDIR/${LGPFX}subscribe_*_f??_??.log  | grep -v '^1 ' >$missedreport
+  missed_dispositions="`wc -l <$missedreport`"
+
+}
+
