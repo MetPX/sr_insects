ab97bb8b09d24a1719b77648b9b6fe6a10415733
PeterSilva <peter@bsqt.homeip.net>
2021-04-24 11:02:46 -0400

    remove duplicates from sample bulletin set as prep for transform_use case. update flow_check to reflect change in callback naming.

--- static_flow/flow_include.sh
+++ static_flow/flow_include.sh
@@ -254,7 +254,7 @@ fi
 
   #countthem "`grep -a '\[INFO\] post_log notice' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
   if [ "${sarra_py_version:0:1}" == "3" ]; then
-      countthem "`grep -a 'Message received:' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
+      countthem "`grep -a 'after_accept accepted:' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
       totsarx="${tot}"
       countthem "`grep -a 'putNewMessage published' "$LOGDIR"/${LGPFX}sarra_download_f20_*.log | wc -l`"
       totsarp="${tot}"
