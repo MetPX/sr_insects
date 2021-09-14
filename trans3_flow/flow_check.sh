aa89a30a8b24b4178e1b9e20702dc32fec41aca9
PeterSilva <peter@bsqt.homeip.net>
2021-09-13 23:51:07 -0400

    Adding v3 specific trans3_flow.

---
+++ trans3_flow/flow_check.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+# parse arguments
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -s|--skip_summaries)
+    skip_summaries=true
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}"
+
+. ./flow_include.sh
+countall
+
+function summarize_performance {
+    path="$LOGDIR"/$1
+    shift
+    pattern=$1
+    shift
+    for i in $* ; do
+       best_fn=''
+       printf "\n\t$i\n\n"
+       for j in ${path}_${i}_*.log; do
+           msg="`grep ${pattern} ${j} | tail -1`"
+           if [[ -z "$msg" ]]; then
+               continue
+           fi
+           fn=`echo $(basename ${j}) | awk -F'.' '{print $3}'`
+           if [[ -z "$fn" ]]; then
+               best_fn=`echo $(basename ${j})`
+               echo "`basename $j` ${msg}"
+           elif [[ -z "$best_fn" ]]; then
+               echo "`basename $j` ${msg}"
+           fi
+       done
+    done
+}
+
+function summarize_logs {
+    printf "\n$1 Summary:\n"
+    input_size=${#1}
+    fcl="$LOGDIR"/flowcheck_$1_logged.txt
+    msg_counts=`grep -h -o "\[$1\] *.*" "$LOGDIR"/*.log | sort | uniq -c -w"$((input_size+20))" | sort -n -r`
+    echo '' > ${fcl}
+
+    if [[ -z $msg_counts ]]; then
+       echo NO $1S IN LOGS
+    else
+       backup_ifs=$IFS
+       IFS=$'\n'
+       for msg_line in $msg_counts; do
+            count=`echo ${msg_line} | awk '{print $1}'`
+            msg=`echo ${msg_line} | sed "s/^ *[0-9]* \[$1\] *//g"`
+            pattern="\[$1\] *${msg}"
+            filelist=($(grep -l ${pattern::$((input_size + 22))} "$LOGDIR"/*.log))
+            if [[ ${filelist[@]} ]]; then
+                first_filename=`basename ${filelist[0]} | sed 's/ /\n/g' | sed 's|.*\/||g' | sed 's/_[0-9][0-9]\.log\|.log//g' | uniq`
+                files_nb=${#filelist[@]}
+                echo "  ${count}"$'\u2620'"${first_filename}"$'\u2620'"(${files_nb} file)"$'\u2620'"`echo ${msg_line} | sed "s/^ *[0-9]* //g"`" >> ${fcl}
+                echo ${filelist[@]} | sed 's/^//g' | sed 's/ \//\n\//g' >> ${fcl}
+                echo -e >> ${fcl}
+            fi
+       done
+       IFS=${backup_ifs}
+       result=`grep -c $1 ${fcl}`
+       if [[ ${result} -gt 10 ]]; then
+           grep $1 ${fcl} | head | column -t -s $'\u2620' | cut -c -130
+           echo
+           echo "More than 10 TYPES OF $1S found... for the rest, have a look at $fcl for details"
+       else
+           grep $1 ${fcl} | column -t -s $'\u2620' | cut -c -130
+       fi
+    fi
+}
+
+if [[ -z "$skip_summaries" ]]; then
+    # PAS performance summaries
+    printf "\nDownload Performance Summaries:\tLOGDIR=$LOGDIR\n"
+    summarize_performance ${LGPFX}sarra msg_total: download_f20
+    summarize_performance ${LGPFX}subscribe file_total: u_sftp_f60 ftp_f70 q_f71
+
+    echo
+    # MG shows retries
+    echo
+
+    if [[ ! "$SARRA_LIB" ]]; then
+       echo NB retries for ${LGPFX}sender    `grep Retrying "$LOGDIR"/${LGPFX}sender*.log | wc -l`
+    else
+       echo NB retries for "$SARRA_LIB"/${LGPFX}sender.py    `grep Retrying "$LOGDIR"/${LGPFX}sender*.log | wc -l`
+    fi
+
+    summarize_logs ERROR
+    summarize_logs WARNING
+fi
+
+passedno=0
+tno=0
+
+if [[ "${totshovel2}" -gt "${totshovel1}" ]]; then
+   maxshovel=${totshovel2}
+else 
+   maxshovel=${totshovel1}
+fi
+printf "\n\tMaximum of the shovels is: ${maxshovel}\n\n"
+
+printf "\t\tTEST RESULTS\n\n"
+
+tot2shov=$(( ${totshovel1} + ${totshovel2} ))
+
+tot2stat=$(( 2*${staticfilecount} ))
+
+echo "                 | static post |"
+#calcres "${staticfilecount}" "${totshovel2}" "${LGPFX}post\t count of posted files (${totshovel2}) should be same those in the static data directory\t (${staticfilecount})"
+calcres "${totshovel1}" "${totshovel2}" "${LGPFX}post\t (${totshovel1}) t_dd1 should have the same number of items as t_dd2\t (${totshovel2})"
+echo "                 | sarra download and transform a subset |"
+calcres "${totsarx}" "${tot2stat}" "${LGPFX}sarra\t (${totsarx}) should receive the same number of items as both static file trees (${tot2stat})"
+calcres "${totsarp}" "${staticfilecount}" "${LGPFX}sarra\t (${totsarp}) should publish the same number of items as the data directory\t (${staticfilecount})"
+calcres "${totrejected}" "${staticfilecount}" "${LGPFX}sarra\t (${totrejected}) should reject the same number of items as static tree\t (${staticfilecount})"
+calcres "${totsubu}" "${totsent}"  "${LGPFX}subscribe u_sftp_f60 (${totsubu}) should download same number of items as ${LGPFX}sender (${totsent})"
+calcres "${totsubcp}" "${totsent}" "${LGPFX}subscribe cp_f61\t (${totsubcp}) should download same number of items as ${LGPFX}sender (${totsent})"
+echo "                 | poll       routing |"
+calcres "${totpoll1}" "${totsent}" "${LGPFX}poll f62\t (${totpoll1}) should publish same number of items of ${LGPFX}sender sent\t (${totsent})"
+calcres "${totsubq}" "${totpoll1}" "${LGPFX}subscribe q_f71\t (${totsubq}) should download same number of items as ${LGPFX}poll test1_f62 (${totpoll1})"
+echo "                 | flow_post  routing |"
+calcres "${totpost1}" "${totsent}" "${LGPFX}post test2_f61\t (${totpost1}) should have the same number of items of ${LGPFX}sender \t (${totsent})"
+calcres "${totsubftp}" "${totpost1}" "${LGPFX}subscribe ftp_f70\t (${totsubftp}) should have the same number of items as ${LGPFX}post test2_f61 (${totpost1})"
+calcres "${totpost1}" "${totshimpost1}" "${LGPFX}post test2_f61\t (${totpost1}) should have about the same number of items as shim_f63\t (${totshimpost1})"
+zerowanted "${missed_dispositions}" "${maxshovel}" "messages received that we don't know what happened."
+
+# these almost never are the same, and it's a problem with the post test. so failures here almost always false negative.
+#calcres ${totpost1} ${totsubu} "post test2_f61 ${totpost1} and subscribe u_sftp_f60 ${totsubu} run together. Should be about the same."
+
+
+tallyres "${tno}" "${passedno}" "Overall ${passedno} of ${tno} passed (sample size: $staticfilecount) !"
+results=$?
+
+if (("${missed_dispositions}">0)); then
+   # PAS missed_dispositions means definite Sarra bug, very serious.
+   echo "Please review $missedreport"
+   results=1
+fi
+echo
+
+exit ${results}
---
+++ trans3_flow/flow_post.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# script to be started by flow_setup.sh which runs sr_post in the background.
+
+#adding libcshim posting as well.
+# change LD_PRELOAD with path to libsrshim if not using system one
+export SR_POST_CONFIG="$CONFDIR/post/shim_f63.conf"
+# The directory we run the flow test scripts in...
+tstdir="`pwd`"
+httpdocroot=`cat $tstdir/.httpdocroot`
+
+if [ ! -d ${httpdocroot} ]; then
+   exit
+fi
+
+. ../flow_utils.sh
+
+if [ ! -d ${httpdocroot}/posted_by_shim ]; then
+   mkdir  ${httpdocroot}/posted_by_shim
+fi
+
+if [[ ":$SARRA_LIB/../:" != *":$PYTHONPATH:"* ]]; then
+    if [ "${PYTHONPATH:${#PYTHONPATH}-1}" == ":" ]; then
+        export PYTHONPATH="$PYTHONPATH$SARRA_LIB/../"
+    else 
+        export PYTHONPATH="$PYTHONPATH:$SARRA_LIB/../"
+    fi
+fi
+
+# sr_post initial start
+srpostdir=`cat $tstdir/.httpdocroot`/sent_by_tsource2send
+srpostlstfile=$httpdocroot/srpostlstfile
+srpostlstfile_new=$httpdocroot/srpostlstfile.new
+srpostlstfile_old=$httpdocroot/srpostlstfile.old
+
+echo > ${srpostlstfile_old}
+# sr_post call
+
+function do_sr_post {
+
+   cd $srpostdir
+   # sr_post testing START
+   # TODO - consider if .httpdocroot ends with a '/' ?
+   find . -type f -print | grep -v '.tmp$'  > $srpostlstfile
+   find . -type l -print | grep -v '.tmp$' >> $srpostlstfile
+   cat $srpostlstfile    | sort > $srpostlstfile_new
+
+   # Obtain file listing delta
+   rm    /tmp/diffs.txt 2> /dev/null
+   touch /tmp/diffs.txt
+   comm -23 $srpostlstfile_new $srpostlstfile_old > /tmp/diffs.txt
+   srpostdelta=`cat /tmp/diffs.txt`
+   srpostdelta="`echo ${srpostdelta} | tr '\n' ' '`"
+
+   # | sed 's/^..//'
+   if [ "$srpostdelta" == "" ]; then
+    return
+   fi
+
+   # loop on each line to properly post filename with space *** makes too much load on CPU ***
+
+   # cant seem to have success to have .S P C files in /tmp/diffs.txt and have them as correct arguments
+   # theses commands would not succeed :
+   #cat /tmp/diffs.txt | sed 's/\(.*S P C\)/"\1"/' | sed 's/S P C/S\\ P\\ C/' > /tmp/diffs2.txt
+   #cat /tmp/diffs.txt | sed "s/\(.*S P C\)/'\1'/" > /tmp/diffs2.txt
+   #cat /tmp/diffs.txt | sed "s/\(.*S P C\)/'\1'/" | sed 's/S P C/S\\ P\\ C/' > /tmp/diffs2.txt
+   # | sed '/slink$/d' | sed '/moved$/d' | sed '/hlink$/d' | sed '/tmp$/d'
+
+
+   if [ "${sarra_py_version:0:1}" == "3" ]; then
+      POST=sr3_post
+      SHIMLIB="libsr3shim.so.1.0.0"
+   else
+      POST=sr_post
+      SHIMLIB="libsrshim.so.1.0.0"
+   fi
+
+   if [ ! "$SARRA_LIB" ]; then
+    $POST -c test2_f61.conf -p $srpostdelta
+   else 
+    "$SARRA_LIB"/sr_post.py -c "$CONFDIR"/post/test2_f61.conf -p ${srpostdelta}
+   fi
+   cd $srpostdir  
+   if [ "$SARRAC_LIB" ]; then
+    LD_PRELOAD="$SARRAC_LIB/${SHIMLIB}" cp -p --parents ${srpostdelta} ${httpdocroot}/posted_by_shim
+   else 
+    LD_PRELOAD="${SHIMLIB}" cp -p --parents ${srpostdelta} ${httpdocroot}/posted_by_shim
+   fi
+   
+   cp -p $srpostlstfile_new $srpostlstfile_old
+}
+
+# sr_post initial end
+
+while true; do
+   sleep 1
+   do_sr_post
+done
+
