a26c5e19ca14c4d012cc25ff852b5373d4903e02
Peter Silva <peter@bsqt.homeip.net>
2023-03-31 15:23:47 -0400

    missing a line feed.

--- dynamic_flow/flow_limit3.sh
+++ dynamic_flow/flow_limit3.sh
@@ -123,7 +123,7 @@ sleep 10
    queued_msgcnt="`rabbitmqadmin -H localhost -u bunnymaster -p ${adminpw} -f tsv list queues | awk ' BEGIN {t=0;} (NR > 1)  && /_f[0-9][0-9]/ { t+=$2; }; END { print t; };'`"
    while [ ${queued_msgcnt} -gt 0 ]; do
         queues_with_msgs="`rabbitmqadmin -H localhost -u bunnymaster -p ${adminpw} -f tsv list queues | awk ' BEGIN {t=0;} (NR > 1)  && /_f[0-9][0-9]/ && ( $2 > 0 ) { print $1; };'`"
-        printf "Still %4s messages (in queues: %s) flowing, waiting..." "$queued_msgcnt" "$queues_with_messages"
+        printf "Still %4s messages (in queues: %s) flowing, waiting...\n" "$queued_msgcnt" "$queues_with_messages"
         sleep 35
         queued_msgcnt="`rabbitmqadmin -H localhost -u bunnymaster -p ${adminpw} -f tsv list queues | awk ' BEGIN {t=0;} (NR > 1)  && /_f[0-9][0-9]/ { t+=$2; }; END { print t; };'`"
    done
