03e2512795f8638fcd3230668ba9fe317e6d5bb2
Peter Silva <peter@bsqt.homeip.net>
2023-05-27 01:06:07 -0400

    wget was wrong.

--- static_flow/flow_maint_test.sh
+++ static_flow/flow_maint_test.sh
@@ -42,15 +42,15 @@ qchk 15 "queues existing after declare"
 xchk "exchanges extant after declare"
 
 printf "ls ${HOME}: `ls ${HOME}`\n\n" 
-pyexamples=${HOME}/work/sarracenia/sarracenia/examples
+pyexamples=${HOME}/work/sarracenia/examples
 if [ ! -d "${pyexamples}" ]; then
     pyexamples=${HOME}/Sarracenia/sr3/sarracenia/examples
     if [ ! -d "${pyexamples}" ]; then
 	if [ ! "${GITHUB_BASE_REF}"]; then
             GITHUB_BASE_REF=v03_wip
 	fi
-        wget -o moth_api_producer.py https://github.com/MetPX/sarracenia/blob/${GITHUB_BASE_REF}/sarracenia/examples/moth_api_producer.py 
-        wget -o moth_api_consumer.py https://github.com/MetPX/sarracenia/blob/${GITHUB_BASE_REF}/sarracenia/examples/moth_api_consumer.py 
+        wget https://github.com/MetPX/sarracenia/blob/${GITHUB_BASE_REF}/sarracenia/examples/moth_api_producer.py 
+        wget https://github.com/MetPX/sarracenia/blob/${GITHUB_BASE_REF}/sarracenia/examples/moth_api_consumer.py 
         pyexamples=`pwd`
     fi
 fi
