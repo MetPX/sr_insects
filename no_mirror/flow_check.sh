dc14643bd49fbb5eb970850df16befc3fd97e140
Peter Silva <peter@bsqt.homeip.net>
2023-12-19 12:49:33 -0500

    sort fix for checktree for no_mirror test

--- no_mirror/flow_check.sh
+++ no_mirror/flow_check.sh
@@ -89,7 +89,7 @@ function checktree {
 
   report=${SUMDIR}/`basename ${tree}`.txt
   #if [ ! -f ${report} ]; then
-  (cd ${tree}; find . \! -type d | xargs md5sum ) > ${report}
+  (cd ${tree}; find . \! -type d | xargs md5sum ) |sort > ${report}
   #fi
 
 }
