1feea2053d61e3fc72c27624ec526cb89332786a
Peter Silva <peter@bsqt.homeip.net>
2024-04-29 15:45:52 -0400

    Revert "output changed... odd"
    was breakage.
    This reverts commit 46731f36892e64786d6259321f350eb1a1bb6524.

--- static_flow/flow_include.sh
+++ static_flow/flow_include.sh
@@ -205,7 +205,7 @@ function countall {
   totfilewatch="${tot}"
 
   if [[ "${sarra_py_version}" > "3.00.52" ]]; then
-      countthem "`grep -ae 'a directory relPath' ${LOGDIR}/${LGPFX}watch_f40_*.log | wc -l`"
+      countthem "`grep -a 'a directory with' ${LOGDIR}/${LGPFX}watch_f40_*.log | wc -l`"
       totdirwatch="${tot}"
 
       countthem "`sr3 status | grep -a 'wVip' | wc -l `"
